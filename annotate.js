594117a957a0729d91ca22c0624a7fbfe886587d
Bhavin Jawade <bhavinja@ib.cedar.buffalo.edu>
2021-11-18 15:11:39 -0500

    annotation tool

--- annotate.js
+++ annotate.js
@@ -2,143 +2,168 @@ var width = window.innerWidth;
 var height = window.innerHeight;
 
 var stage = new Konva.Stage({
-container: 'container',
-width: width,
-height: height,
+    container: 'container',
+    width: width,
+    height: height,
 });
 
 var layer = new Konva.Layer();
 stage.add(layer);
 
 var imageObj = new Image();
-imageObj.onload = function() {
-  var image = new Konva.Image({
-    x: 0,
-    y: 0,
-    image: imageObj,
-    width: width,
-    height: height
-  });
-  layer.add(image)
-
-  var rect1 = new Konva.Rect({
-    x: 60,
-    y: 60,
-    width: 100,
-    height: 90,
-    fill: 'transparent',
-    name: 'rect',
-    draggable: true,
+imageObj.onload = function () {
+    var image = new Konva.Image({
+        x: 0,
+        y: 0,
+        image: imageObj,
+        width: width,
+        height: height
     });
-    layer.add(rect1);
-  
-var tr = new Konva.Transformer();
-var borderStrokeWidth = tr.borderStrokeWidth();
-tr.borderStrokeWidth(3);
-layer.add(tr);
-// by default select all shapes
-tr.nodes([rect1]);
+    layer.add(image)
 
-// add a new feature, lets add ability to draw selection rectangle
-var selectionRectangle = new Konva.Rect({
-fill: 'rgba(0,0,255,0.5)',
-visible: false,
-});
-layer.add(selectionRectangle);
+    // add a new feature, lets add ability to draw selection rectangle
+    var selectionRectangle = new Konva.Rect({
+        fill: 'rgba(0,0,255,0.5)',
+        visible: false,
+    });
+    layer.add(selectionRectangle);
 
-var x1, y1, x2, y2;
-stage.on('mousedown touchstart', (e) => {
-// do nothing if we mousedown on any shape
-if (e.target !== stage) {
-    return;
-}
-e.evt.preventDefault();
-x1 = stage.getPointerPosition().x;
-y1 = stage.getPointerPosition().y;
-x2 = stage.getPointerPosition().x;
-y2 = stage.getPointerPosition().y;
+    var x1, y1, x2, y2;
+    stage.on('mousedown touchstart', (e) => {
+        // do nothing if we mousedown on any shape
+        if (e.target !== stage) {
+            return;
+        }
+        e.evt.preventDefault();
+        x1 = stage.getPointerPosition().x;
+        y1 = stage.getPointerPosition().y;
+        x2 = stage.getPointerPosition().x;
+        y2 = stage.getPointerPosition().y;
 
-selectionRectangle.visible(true);
-selectionRectangle.width(0);
-selectionRectangle.height(0);
-});
+        selectionRectangle.visible(true);
+        selectionRectangle.width(0);
+        selectionRectangle.height(0);
+    });
 
-stage.on('mousemove touchmove', (e) => {
-// do nothing if we didn't start selection
-if (!selectionRectangle.visible()) {
-    return;
-}
-e.evt.preventDefault();
-x2 = stage.getPointerPosition().x;
-y2 = stage.getPointerPosition().y;
+    stage.on('mousemove touchmove', (e) => {
+        // do nothing if we didn't start selection
+        if (!selectionRectangle.visible()) {
+            return;
+        }
+        e.evt.preventDefault();
+        x2 = stage.getPointerPosition().x;
+        y2 = stage.getPointerPosition().y;
 
-selectionRectangle.setAttrs({
-    x: Math.min(x1, x2),
-    y: Math.min(y1, y2),
-    width: Math.abs(x2 - x1),
-    height: Math.abs(y2 - y1),
-});
-});
+        selectionRectangle.setAttrs({
+            x: Math.min(x1, x2),
+            y: Math.min(y1, y2),
+            width: Math.abs(x2 - x1),
+            height: Math.abs(y2 - y1),
+        });
+    });
 
-stage.on('mouseup touchend', (e) => {
-// do nothing if we didn't start selection
-if (!selectionRectangle.visible()) {
-    return;
-}
-e.evt.preventDefault();
-// update visibility in timeout, so we can check it in click event
-setTimeout(() => {
-    selectionRectangle.visible(false);
-});
+    stage.on('mouseup touchend', (e) => {
+        // do nothing if we didn't start selection
+        if (!selectionRectangle.visible()) {
+            return;
+        }
+        e.evt.preventDefault();
+        // update visibility in timeout, so we can check it in click event
+        setTimeout(() => {
+            selectionRectangle.visible(false);
+        });
 
-var shapes = stage.find('.rect');
-var box = selectionRectangle.getClientRect();
-var selected = shapes.filter((shape) =>
-    Konva.Util.haveIntersection(box, shape.getClientRect())
-);
-tr.nodes(selected);
-});
+        var shapes = stage.find('.rect');
+        var box = selectionRectangle.getClientRect();
+        var selected = shapes.filter((shape) =>
+            Konva.Util.haveIntersection(box, shape.getClientRect())
+        );
+        tr.nodes(selected);
+    });
 
-// clicks should select/deselect shapes
-stage.on('click tap', function (e) {
-// if we are selecting with rect, do nothing
-if (selectionRectangle.visible()) {
-    return;
-}
+    function createRect(x, y){
+        console.log("Inside createRect")
+        var rect2 = new Konva.Rect({
+            x: x,
+            y: y,
+            width: 100,
+            height: 90,
+            fill: 'transparent',
+            name: 'rect',
+            draggable: true,
+        });
 
-// if click on empty area - remove all selections
-if (e.target === stage) {
-    tr.nodes([]);
-    return;
-}
+        var button = new Konva.Label({
+            x: x,
+            y: y,
+            opacity: 0.75,
+          });
+          button.add(new Konva.Tag({
+            fill: 'white',
+            lineJoin: 'round',
+            shadowColor: 'white',
+            shadowBlur: 10,
+            shadowOffset: 10,
+            shadowOpacity: 0.5
+          }));
+          
+          button.add(new Konva.Text({
+            text: 'X',
+            fontSize: 18,
+            padding: 5,
+            fill: 'black'
+          }));
+          
+          
+          button.on('click', () => {
+              alert('clicked on canvas button');
+          })
 
-// do nothing if clicked NOT on our rectangles
-if (!e.target.hasName('rect')) {
-    return;
-}
+        layer.add(rect2);
+        layer.add(button);      
 
-// do we pressed shift or ctrl?
-const metaPressed = e.evt.shiftKey || e.evt.ctrlKey || e.evt.metaKey;
-const isSelected = tr.nodes().indexOf(e.target) >= 0;
+        var tr1 = new Konva.Transformer();
+        tr1.borderStrokeWidth(3);
+        tr1.borderStroke("white")
+        layer.add(tr1);
+        // by default select all shapes
+        var nodelist = [rect2, button]
+        tr1.nodes(nodelist);
 
-if (!metaPressed && !isSelected) {
-    // if no key pressed and the node is not selected
-    // select just one
-    tr.nodes([e.target]);
-} else if (metaPressed && isSelected) {
-    // if we pressed keys and node was selected
-    // we need to remove it from selection:
-    const nodes = tr.nodes().slice(); // use slice to have new copy of array
-    // remove node from array
-    nodes.splice(nodes.indexOf(e.target), 1);
-    tr.nodes(nodes);
-} else if (metaPressed && !isSelected) {
-    // add the node into selection
-    const nodes = tr.nodes().concat([e.target]);
-    tr.nodes(nodes);
-}
-});
-  
+        button.on('click tap', function (e) {
+            tr1.destroy();
+            rect2.destroy();
+            button.destroy();
+            layer.draw();
+        });
+    }
+
+    // clicks should select/deselect shapes
+    stage.on('click tap', function (e) {
+        console.log(e)
+        // if we are selecting with rect, do nothing
+        if (selectionRectangle.visible()) {
+            console.log("Rectangle")
+            return;
+        }
+
+        // if click on empty area - remove all selections
+        if (e.target === stage) {
+            console.log("Empty Area")
+            tr.nodes([]);
+            return;
+        }
+
+        // do nothing if clicked NOT on our rectangles
+
+        if (e.target.className == 'Image') {
+            console.log("Empty Area - not rect", e)
+            var pos = stage.getPointerPosition();
+            createRect(pos.x, pos.y)
+            return;
+        }
+    });
+
 };
 imageObj.src = 'https://previews.123rf.com/images/posinote/posinote1711/posinote171100095/91013749-mixed-many-type-of-fruits-with-full-frame-and-vertical-photo-.jpg'
 
