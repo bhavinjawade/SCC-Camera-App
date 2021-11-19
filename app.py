01fda7f0038e2c16e5de7aa8e38e2f52db641429
Bhavin Jawade <bhavinjawade@Bhavins-MacBook-Air.local>
2021-11-19 03:20:30 -0500

    server added

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, request, jsonify
 import base64
+import sys
+
 BASE_ADR = "../"
 app = Flask(__name__)
 
 @app.route('/scc_server_receive/', methods=['GET', 'POST'])
-def welcome():
+def scc_server_receive():
     content = request.form
+    print(content, file=sys.stderr)
     for filename, b64 in content.items():
         with open(BASE_ADR + "/image_dataset/" + filename + ".png", "wb") as fh:
+            print(b64)
             fh.write(base64.decodebytes(b64))    
     return {"status": 200}
 
