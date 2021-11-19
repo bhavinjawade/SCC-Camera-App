9e88d77515021bd72224745d75f5dd2d6fa6ee59
Bhavin Jawade <bhavinjawade@Bhavins-MacBook-Air.local>
2021-11-19 03:05:36 -0500

    server added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Set constraints for the video stream
 var constraints = { video: { facingMode: {exact: 'environment'}}, audio: false };
 var track = null;
+var fd = new FormData();
 
 // Define constants
 const cameraView = document.querySelector("#camera--view"),
@@ -20,18 +21,45 @@ function cameraStart() {
             console.error("Oops. Something is broken.", error);
         });
 }
-
+count = 0
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
+    count += 1
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    console.log(cameraOutput.src)
     cameraOutput.classList.add("taken");
     console.log(cameraOutput);
     document.getElementById('camera--output').style.display = "block";
+    fd.append('image_' + count, cameraOutput.src)
     // track.stop();
 };
 
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
+
+function sendImages(){
+    let headers = new Headers();
+
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'application/json');
+    headers.append('Origin','https://07f1-68-133-40-138.ngrok.io/scc_server_receive');
+
+    var req = fetch('https://07f1-68-133-40-138.ngrok.io/scc_server_receive', {
+        method: 'POST',
+        body: fd, /* or aFile[0]*/
+        mode: 'no-cors',
+        credentials: 'include',
+        headers: headers
+      }); // returns a promise
+      
+      req.then(function(response) {
+        if (response.ok) {
+        } else {
+        }
+      }, function(error) {
+        console.error('failed due to network error or cross domain')
+      })
+}
---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+import base64
+BASE_ADR = "../"
+app = Flask(__name__)
+
+@app.route('/scc_server_receive/', methods=['GET', 'POST'])
+def welcome():
+    content = request.form
+    for filename, b64 in content.items():
+        with open(BASE_ADR + "/image_dataset/" + filename + ".png", "wb") as fh:
+            fh.write(base64.decodebytes(b64))    
+    return {"status": 200}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=105)
