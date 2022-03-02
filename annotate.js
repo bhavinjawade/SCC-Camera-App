d051f87e7a23761de3dded6103837b2e0b234e2b
Bhavin Jawade <bhavinja@ib.cedar.buffalo.edu>
2022-03-01 21:00:47 -0500

    ngrok

--- annotate.js
+++ annotate.js
@@ -4,7 +4,7 @@ var open = false;
 var ImageAnnotations = {}
 var currentImage = ""
 
-var base_app_url = "https://128.205.33.32:5000/"
+var base_app_url = "https://286d-128-205-33-32.ngrok.io"
 
 fetch('./fruits.txt')
   .then(response => response.text())
