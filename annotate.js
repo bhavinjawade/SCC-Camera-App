a51e1ae3165c4cf042260bce1bbf683d322247c9
Bhavin Jawade <bhavinja@ib.cedar.buffalo.edu>
2022-03-01 21:48:35 -0500

    checkboxes

--- annotate.js
+++ annotate.js
@@ -13,8 +13,8 @@ var latitude = ""
 var longitude = ""
 function setLoc(pos) {
     console.log(pos)
-    var latitude = pos.coords.latitude;
-    var longitude = pos.coords.longitude;
+    latitude = pos.coords.latitude;
+    longitude = pos.coords.longitude;
 }
 
 fetch('./fruits.txt')
