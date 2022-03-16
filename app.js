701b02ba693ae5b76113f918a9a3ba39f28d81f5
Bhavin Jawade <bhavinja@ib.cedar.buffalo.edu>
2022-03-16 16:02:34 -0400

    rotation lock

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+function hideAddressBar()
+{
+  if(!window.location.hash)
+  {
+      if(document.height < window.outerHeight)
+      {
+          document.body.style.height = (window.outerHeight + 50) + 'px';
+      }
+
+      setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
+  }
+}
+
+window.addEventListener("load", function(){ if(!window.pageYOffset){ hideAddressBar(); } } );
+window.addEventListener("orientationchange", hideAddressBar );
+
 // Set constraints for the video stream
 var constraints = { video: { facingMode: {exact: 'environment'}}, audio: false };
 var track = null;
